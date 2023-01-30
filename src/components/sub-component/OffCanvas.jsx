c0412849bad4ad5e52ba746368af8deb4084fc08
uka yura <ukayura@ukas-MacBook-Air.local>
2023-01-30 08:41:39 +0800

    update spec

--- src/components/sub-component/OffCanvas.jsx
+++ src/components/sub-component/OffCanvas.jsx
@@ -2,45 +2,71 @@ import axios from "axios";
 import { useState } from "react";
 import CloseIcon from "../../icons/CloseIcon";
 import "../../style/mainStyle/offcanvas.css";
+
 export default function OffCanvas(prop) {
   const { closeOffCanva, data, setProduct } = prop;
-  console.log(data);
-  const [spec, setspec] = useState([]);
+  let [spect, setspec] = useState([]);
   const [addSpecKey, setAddSpecKey] = useState();
   const [addSpecVal, setAddSpecVal] = useState();
+
+  let newSpec = [];
+
+  if (data) {
+    data.spec.map((e) => {
+      newSpec.push(e);
+    });
+  }
+  function updateSpec(e) {
+    let label = e.target.labels[0].innerText;
+    let val = e.target.value;
+
+    newSpec.map((e) => {
+      if (Object.keys(e) == label) {
+        e[label] = val;
+      }
+    });
+  }
   function addedSpec(e) {
     e.preventDefault();
 
-    let specObject = { addSpecKey, addSpecVal };
-
-    setspec([...spec, specObject]);
-    console.log(spec);
+    let specObject = {};
+    specObject[addSpecKey] = addSpecVal;
+    setspec([...spect, specObject]);
+    console.log(spect);
   }
   function handlerSubmit(e) {
     e.preventDefault();
-    console.log(e, "working");
     let name = e.target.name.value;
     let price = e.target.price.value;
     let stock = e.target.stock.value;
     let sale = e.target.sale.value;
     let category = e.target.chooseCategory.value;
     let brand = e.target.chooseBrand.value;
-    let image = data.image;
-    let spec = spec;
-    let id = data.id;
-    let proObject = {
-      name,
-      image,
-      price,
-      stock,
-      sale,
-      category,
-      brand,
-      spec,
-      id,
-    };
-
-    setProduct(undefined);
+    let image = data && data.image;
+    let id = data && data.id;
+    let spec = data ? newSpec : spect;
+    let proObject = data
+      ? {
+          name,
+          image,
+          price,
+          stock,
+          sale,
+          category,
+          brand,
+          spec,
+          id,
+        }
+      : {
+          name,
+          price,
+          stock,
+          sale,
+          category,
+          brand,
+          spec,
+        };
+    console.log(proObject);
     if (data) {
       axios
         .put(`http://localhost:2020/product/${data.id}`, proObject)
@@ -53,9 +79,10 @@ export default function OffCanvas(prop) {
       console.log(proObject);
     }
     closeOffCanva(false);
-    // location.reload();
+    location.reload();
+    setProduct(undefined);
   }
-  console.log(data);
+  // console.log(data);
 
   return (
     <div className="offcanvas">
@@ -120,19 +147,19 @@ export default function OffCanvas(prop) {
                         type="text"
                         name={Object.values(spec)}
                         defaultValue={Object.values(spec)}
+                        onChange={updateSpec}
                       />
                     </label>
                   );
                 })
-              : spec.map((newspec, i) => {
-                  console.log(newspec.addSpecKey);
+              : spect.map((newspec, i) => {
                   return (
                     <label key={i}>
-                      <p>{newspec.addSpecKey} </p>
+                      <p>{Object.keys(newspec)}</p>
                       <input
                         type="text"
-                        name={newspec.specKey}
-                        defaultValue={newspec.addSpecVal}
+                        name={Object.values(newspec)}
+                        defaultValue={Object.values(newspec)}
                       />
                     </label>
                   );
@@ -168,6 +195,7 @@ export default function OffCanvas(prop) {
             <label>
               <p>Категори сонгох</p>
               <select name="chooseCategory" id="selectCategory">
+                {data && <option value={data.category}>{data.category}</option>}
                 <option value="laptop">Laptop</option>
                 <option value="tablet">Tablet</option>
                 <option value="telescop">telescop</option>
@@ -177,7 +205,7 @@ export default function OffCanvas(prop) {
             </label>
             <label>
               <p>Брэнд сонгох</p>
-              <select name="chooseBrand" id="selectCategory">
+              <select name="chooseBrand" id="chooseBrand">
                 <option value="laptop">samsung</option>
                 <option value="tablet">apple</option>
                 <option value="telescop">dell</option>
